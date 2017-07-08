aa31d807727308dd0c26cae81c48c4f75962878b
Xrqton <atarari@gmail.com>
2017-07-08 16:00:29 +0200

    Premier miaou commit

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        if (null == player)
+        {
+            Debug.LogError("player is not assigned. Aborting");
+            return;
+        }
+            
+
+        offset = transform.position - player.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        if (null == player)
+            return;
+
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    float moveHorizontal;
+    float moveVertical;
+
+    ////----------------Correpond à la boule sur laquelle nous allons exercer une force
+    private Rigidbody rb;
+
+    //----------------Lance la première frame du jeu
+    void Start()
+    {
+        //Permet de récupérer la référence du RB et jouer avec
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+    }
+
+    ////----------------Permet de lancer tout ce qui calcul physique 
+    void FixedUpdate()
+    {
+
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Application de la force sur la boule Rigibody e        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+}
