e6d56cd01e007bae35893dcca0115e007a1a4094
Xrqton <atarari@gmail.com>
2017-07-14 16:02:30 +0200

    Script de rotation des pick ups

---
+++ Assets/Script/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        //Deltatime pour que la rotation soit indépendante de la frame, sinon c'est trop rapide
+        transform.Rotate(new Vector3(15, 30, 45)* Time.deltaTime) ;
+	}
+}
